c10b903146822c546bb955800ce43dc60057706e
Augusto Juiz <augustojuizribeiro@gmail.com>
2025-08-20 18:00:42 -0300

    feat: CRUD Church

---
+++ app/Http/Controllers/ChurchController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Interfaces\IChurchService;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ChurchController extends Controller
+{
+    public function __construct(private IChurchService $churchService) {}
+
+    public function index()
+    {
+        return response()->json($this->churchService->getAll());
+    }
+
+    public function show(string $id)
+    {
+        return response()->json($this->churchService->getById($id));
+    }
+
+    public function store(Request $request)
+    {
+        $ufs = 'AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC,SP,SE,TO';
+
+        $data = $request->validate([
+            'name'       => ['required','string','max:120'],
+            'cep'        => ['required','regex:/^\d{5}-?\d{3}$/'],
+            'street'     => ['required','string','max:160'],
+            'number'     => ['required','string','max:10'],
+            'complement' => ['nullable','string','max:160'],
+            'quarter'    => ['required','string','max:120'],
+            'city'       => ['required','string','max:120'],
+            'state'      => ['required',"in:$ufs"],
+        ]);
+
+        return response()->json($this->churchService->create($data), 201);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $ufs = 'AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC,SP,SE,TO';
+
+        $data = $request->validate([
+            'name'       => ['sometimes','string','max:120'],
+            'cep'        => ['nullable','regex:/^\d{5}-?\d{3}$/'],
+            'street'     => ['nullable','string','max:160'],
+            'number'     => ['nullable','string','max:10'],
+            'complement' => ['nullable','string','max:160'],
+            'quarter'    => ['sometimes','string','max:120'],
+            'city'       => ['nullable','string','max:120'],
+            'state'      => ['nullable',"in:$ufs"],
+        ]);
+
+        return response()->json($this->churchService->update($id, $data));
+    }
+
+    public function destroy(string $id)
+    {
+        $this->churchService->delete($id);
+        return response()->json(null, 204);
+    }
+}
--- database/factories/ChurchFactory.php
+++ database/factories/ChurchFactory.php
@@ -15,17 +15,17 @@ class ChurchFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-{
-    return [
-        'name' => $this->faker->company . ' Church',
-        'cep' => $this->faker->postcode,
-        'street' => $this->faker->streetName,
-        'number' => (string) $this->faker->buildingNumber,
-        'complement' => $this->faker->optional()->secondaryAddress,
-        'quarter' => $this->faker->word, // pode ser neighborhood ou district, depende do país
-        'city' => $this->faker->city,
-        'state' => $this->faker->stateAbbr,
-    ];
-}
+    {
+        return [
+            'name' => $this->faker->company . ' Church',
+            'cep' => $this->faker->postcode,
+            'street' => $this->faker->streetName,
+            'number' => (string) $this->faker->buildingNumber,
+            'complement' => $this->faker->optional()->secondaryAddress,
+            'quarter' => $this->faker->word, // pode ser neighborhood ou district, depende do país
+            'city' => $this->faker->city,
+            'state' => $this->faker->stateAbbr,
+        ];
+    }
 
 }
