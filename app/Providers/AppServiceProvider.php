732cdc0992f607cf413012d0d59010ef099bbbc0
Erick-Bonifacio <erickalvarengabonifacio@hotmail.com>
2025-08-30 19:01:38 -0300

    feat: chat crud and tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\ChatService;
+use App\Services\Interfaces\IChatService;
 use Illuminate\Support\ServiceProvider;
 use App\Services\Interfaces\IAreaService;
 use App\Services\Interfaces\IChurchService;
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IChurchService::class, ChurchService::class);
         $this->app->bind(ISongService::class, SongService::class);
         $this->app->bind(IRoleService::class, RoleService::class);
+        $this->app->bind(IChatService::class, ChatService::class);
     }
 
     /**
