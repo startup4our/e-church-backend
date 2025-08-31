5ccc03975930d25c2ebe063234fbb6ab993d477e
Erick-Bonifacio <erickalvarengabonifacio@hotmail.com>
2025-08-31 19:11:03 -0300

    refactor: User creation requires church ID and user have to wait approval

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserStatus;
+use App\Models\Church;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -11,32 +13,27 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
             'birthday' => fake()->date(),
-            'photo_path' => fake()->filePath(),
+            'photo_path' => fake()->imageUrl(), // melhor que filePath()
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'status' => fake()->randomElement([
+                UserStatus::ACTIVE,
+                UserStatus::INACTIVE,
+                UserStatus::WAITING_APPROVAL,
+            ]),
+            'church_id' => Church::factory(), // cria igreja vinculada
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
