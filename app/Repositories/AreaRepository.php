8ab81a0484230a23e3cecb24757b0cedbb20f5f5
Erick-Bonifacio <erickalvarengabonifacio@hotmail.com>
2025-09-18 23:51:05 -0300

    WIP: fixing register church and area retrieve

--- app/Repositories/AreaRepository.php
+++ app/Repositories/AreaRepository.php
@@ -18,9 +18,19 @@ class AreaRepository
         return Area::all();
     }
 
+    public function getByChurchId(int $churchId): Collection
+    {
+        return Area::where('church_id', $churchId)->get();
+    }
+
     public function getById(int $id): Area
     {
         return Area::findOrFail($id);
+    }
+
+    public function getByIdAndChurchId(int $id, int $churchId): Area
+    {
+        return Area::where('id', $id)->where('church_id', $churchId)->firstOrFail();
     }
 
     public function update(int $id, array $data): Area
@@ -30,9 +40,22 @@ class AreaRepository
         return $area;
     }
 
+    public function updateByIdAndChurchId(int $id, int $churchId, array $data): Area
+    {
+        $area = Area::where('id', $id)->where('church_id', $churchId)->firstOrFail();
+        $area->update($data);
+        return $area;
+    }
+
     public function delete(int $id): bool
     {
         $area = Area::findOrFail($id);
+        return $area->delete();
+    }
+
+    public function deleteByIdAndChurchId(int $id, int $churchId): bool
+    {
+        $area = Area::where('id', $id)->where('church_id', $churchId)->firstOrFail();
         return $area->delete();
     }
 
