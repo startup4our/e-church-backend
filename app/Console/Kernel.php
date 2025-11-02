c9c7766dbf6f0a06b1d45dca285dabef992ebf47
Erick-Bonifacio <erickalvarengabonifacio@hotmail.com>
2025-11-02 12:31:26 -0300

    feat: introducing jobs to system: delete old messages and handouts starting

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\HandoutsManagerJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new HandoutsManagerJob())->everyFiveMinutes();
+        $schedule->job(new HandoutsManagerJob())->daily();
     }
 
     /**
---
+++ app/Jobs/DeleteOldMessagesJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Message;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Log;
+
+class DeleteOldMessagesJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info('Delete Old Messages Job has started');
+
+        Message::where('created_at', '<=', now()->subDays(60))->delete();
+
+        Log::info('Delete Old Messages Job has ended');
+    }
+}
